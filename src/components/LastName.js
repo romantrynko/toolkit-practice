7a3dcb4a4bcba9f72b4f8ff9b13cd1fd332c35a3
Roman Trynko <trynkoroman25@gmail.com>
2022-08-26 12:04:14 +0300

    user done

--- src/components/LastName.js
+++ src/components/LastName.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const LastName = () => {
-  return <div className="font-bold">Last Name</div>;
+  const { lastName } = useSelector((state) => state.user);
+  return <div className="font-bold">{lastName}</div>;
 };
 
 export default LastName;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import { render } from 'react-dom';
 import './index.css';
 import App from './App';
+import { store } from './store/store';
+import { Provider } from 'react-redux';
 
 const root = document.getElementById('root');
-render(<App />, root);
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  root
+);
